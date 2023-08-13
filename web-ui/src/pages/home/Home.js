cf75137482af0389f5ed4e5dc459007bc1ff3e8f
Rajitha <rajithat1998@gmail.com>
2023-08-13 22:59:04 +0530

    Add Store

--- web-ui/src/pages/home/Home.js
+++ web-ui/src/pages/home/Home.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import {Button,Stack} from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 
 export default function Home() {
+  const navigate = useNavigate()
   return (
     <div>
-       This is Home Page
+      <Stack>
+      This is Home Page
+       <Button onClick={() => navigate('/about-us')} variant="contained" color="primary">
+        About Us
+      </Button> </Stack>
+
     </div>
   )
 }
